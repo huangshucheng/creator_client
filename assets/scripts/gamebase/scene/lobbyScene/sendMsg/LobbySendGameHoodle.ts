010a21f894323371043eb0fad3c584d304e89992
hcc <827773271@qq.com>
2020-10-21 00:12:30 +0800

    无用删除了

--- assets/scripts/gamebase/scene/lobbyScene/sendMsg/LobbySendGameHoodle.ts
+++ assets/scripts/gamebase/scene/lobbyScene/sendMsg/LobbySendGameHoodle.ts
@@ -14,44 +14,6 @@ export default class LobbySendGameHoodleMsg {
         CellManager.getInstance().start("CellLoginLogic", null, 5);
     }
 
-    //创建房间
-    static send_create_room(gamerule: string){
-        if (gamerule == null || gamerule == ''){
-            return;
-        }
-        let body = { gamerule: gamerule };
-        CellManager.getInstance().start("CellCreateRoom", body, 5);
-    }
-
-    //加入房间
-    static send_join_room(roomid: string){
-        if (roomid == null || roomid == ''){
-            return;
-        }
-        console.log("hcc>>send_join_room: " , roomid);
-        LobbySendGameHoodleMsg.send(GameHoodleProto.XY_ID.eJoinRoomReq, {roomid: roomid});
-    }
-
-    //退出房间
-    static send_exit_room(){
-        LobbySendGameHoodleMsg.send(GameHoodleProto.XY_ID.eExitRoomReq);
-    }
-
-    //解散房间
-    static send_dessolve_room(){
-        LobbySendGameHoodleMsg.send(GameHoodleProto.XY_ID.eDessolveReq);
-    }
-
-    //是否创建了房间
-    static send_get_room_status(){
-        LobbySendGameHoodleMsg.send(GameHoodleProto.XY_ID.eGetRoomStatusReq);
-    }
-
-    //返回房间
-    static send_back_room(){
-        CellManager.getInstance().start("CellBackRoom", null, 5);
-    }
-
     //游戏服务信息
     static send_get_ugame_info(){
         LobbySendGameHoodleMsg.send(GameHoodleProto.XY_ID.eUserGameInfoReq);
