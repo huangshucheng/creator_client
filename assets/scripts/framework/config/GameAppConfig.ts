4de6833b4d5a3288b46bc152d3925a62a6680734
hcc <827773271@qq.com>
2020-11-04 00:43:36 +0800

    客户端配置修改了

--- assets/scripts/framework/config/GameAppConfig.ts
+++ assets/scripts/framework/config/GameAppConfig.ts
@@ -6,11 +6,11 @@ export default class GameAppConfig {
     // static LOCAL_IP              = "127.0.0.1";
     static LOCAL_HOST               = "192.168.31.181";            // 本地电脑ip,暂时写死，主要为了游戏在web平台测试用
     static REMOTE_IP                = "www.hccfun.com";
-    static REMOTE_WECHAT_PORT       = "6081";                       // 小程序wss端口，nginx配置6081,转发到6061
-    static NATIVE_PLATFORM_PORT     = "6061";                       // 安卓IOS原生平台端口,直接连接到6081
+    static REMOTE_WECHAT_PORT       = "5081";                       // 小程序wss端口，nginx配置6081,转发到6061
+    static NATIVE_PLATFORM_PORT     = "5061";                       // 安卓IOS原生平台端口,直接连接到5081
     static PROTO_TYPE               = ProtoManater.PROTO_BUF;   
-    static REMORE_HTTP_HOT_PORT     = "7000";                       // http 热更新端口
-    static REMORE_HTTP_CONFIG_PORT  = "6070";                       // http 配置端口
+    static REMORE_HTTP_HOT_PORT     = "5000";                       // http 热更新端口
+    static REMORE_HTTP_CONFIG_PORT  = "5070";                       // http 配置端口
     static LOCAL_MANIFEST_PATH      = "manifest/project";
     static IS_WECHAT_GAME_ONLINE    = true;                             //微信小游戏线上环境，需要隐藏账号登录注册入口，只能使用微信登录
 
